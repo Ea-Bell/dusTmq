8dcf7eb545ffdbdc50201284a94196e53c9d03b7
Ea-Bell <cjp975@naver.com>
2022-06-21 16:38:45 +0900

    댓글 뺵업용 커밋

--- src/main/java/com/example/dusTmq/domain/comment/Comment.java
+++ src/main/java/com/example/dusTmq/domain/comment/Comment.java
@@ -2,47 +2,43 @@ package com.example.dusTmq.domain.comment;
 
 import com.example.dusTmq.domain.board.BoardDetailVO;
 import com.example.dusTmq.domain.user.Member;
-import com.example.dusTmq.domain.util.BaseTImeEntity;
-import lombok.*;
-import org.hibernate.annotations.CreationTimestamp;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-import org.springframework.format.annotation.DateTimeFormat;
+import com.example.dusTmq.domain.util.BaseTimeEntity;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
-import javax.validation.constraints.NotNull;
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
 
 @Entity
-@Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@Inheritance(strategy =InheritanceType.JOINED)
-@ToString(exclude = {"boardDetailVO", "member", "parent", "childComment"})
-//컴포지션 클래스
-public abstract class Comment extends BaseTImeEntity {
+@Getter
+public class Comment  extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "comment_id")
-    private Long commentId;
+    private Long id;
+    private String content;
+    private int depth; // 깊이
 
-    @ManyToOne(fetch = FetchType.LAZY, targetEntity = BoardDetailVO.class)
-    @JoinColumn(name = "boardDetail_Id")
-    @NotNull
+
+    //부모, 자식에 대한 comment가 필요하다.
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "board_detail_id")
+    @Column(nullable = false)
     private BoardDetailVO boardDetailVO;
 
-    @ManyToOne(fetch = FetchType.LAZY, targetEntity = Member.class)
+    @ManyToOne
     @JoinColumn(name = "member_id")
-    @NotNull
+    @Column(nullable = false)
     private Member member;
 
-    public Comment(BoardDetailVO boardDetailVO, Member member) {
+    @Builder(builderMethodName = "newComment")
+    public Comment(String content, int depth, BoardDetailVO boardDetailVO, Member member) {
+        this.content = content;
+        this.depth = depth;
         this.boardDetailVO = boardDetailVO;
         this.member = member;
     }
 }
-
-
